dd342e0244d2e78309c701d76d1e9a58d56bb469
AbdulWahabRaza123 <abdulwahabraza123@gmail.com>
2023-06-05 01:02:32 +0500

    add encryption to message

--- backend/Controllers/messageControllers.js
+++ backend/Controllers/messageControllers.js
@@ -2,15 +2,37 @@ const asyncHandler = require("express-async-handler");
 const Message = require("../Models/messageModel");
 const User = require("../Models/userModel");
 const Chat = require("../Models/chatModel");
+function encryptText(text) {
+  let encryptedText = "";
+  for (let i = 0; i < text.length; i++) {
+    let charCode = text.charCodeAt(i);
+    let encryptedCharCode = charCode + 1;
+    let encryptedChar = String.fromCharCode(encryptedCharCode);
+    encryptedText += encryptedChar;
+  }
+  return encryptedText;
+}
+function decryptText(encryptedText) {
+  let decryptedText = "";
+  for (let i = 0; i < encryptedText.length; i++) {
+    let encryptedCharCode = encryptedText.charCodeAt(i);
+    let charCode = encryptedCharCode - 1;
+    let decryptedChar = String.fromCharCode(charCode);
+    decryptedText += decryptedChar;
+  }
+  return decryptedText;
+}
 const sendMessage = asyncHandler(async (req, res) => {
   const { content, chatId } = req.body;
   if (!content || !chatId) {
     console.log("invalid data pass to the request");
     return res.statusCode(400);
   }
+  const encryptContent = encryptText(content);
+  console.log("This is encrypted message ", encryptContent);
   var newMessage = {
     sender: req.user._id,
-    content: content,
+    content: encryptContent,
     chat: chatId,
   };
   try {
@@ -26,6 +48,8 @@ const sendMessage = asyncHandler(async (req, res) => {
     await Chat.findByIdAndUpdate(req.body.chatId, {
       latestMessage: message,
     });
+    console.log("This is content of message ", decryptText(message.content));
+    message["content"] = decryptText(message.content);
     res.json(message);
   } catch (e) {
     res.status(400);
